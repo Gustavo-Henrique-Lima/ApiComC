25ef85a87fcf8d617ed8b0bcdfcc246c260eb42c
Gustavo Henrique <gustavohgustavo@hotmail.com>
2023-04-17 21:19:15 -0300

    Classe Livraria criada

---
+++ Models/Livraria.cs
@@ -0,0 +1,11 @@
+namespace Models
+{
+    public class Livraria
+    {
+        public int id{get;set;}
+        public string? name{get;set;}
+        public int qtdPaginas{get;set;}
+        public string? editora{get;set;}
+        public string? genero{get;set;}
+    }
+}
